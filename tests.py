6662da04934f4f600fae5cf7d924f69ff608c1a5
Kristians Artis Berzins <kristiansberzins@outlook.lv>
2021-02-01 13:55:07 +0200

    Izlabots PY fails

--- tests.py
+++ tests.py
@@ -6,7 +6,8 @@ from random import randint
 
 skaitlis = randint(1, 10)
 
-while skaitlis != 10 :
+while skaitlis != 9 :
     print('Tests')
+    skaitlis = randint(1, 10)
 
 print('Programma darbu beidz.')
