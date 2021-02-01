cb28a48f2d707bb4e5c3dc4180d07d784c8bb642
Kristians Artis Berzins <kristiansberzins@outlook.lv>
2021-02-01 13:42:51 +0200

    Pievienots PY fails

---
+++ tests.py
@@ -0,0 +1,12 @@
+'''
+    Testa programma
+    Autors: Kristiāns Artis Bērziņš
+'''
+from random iport randint
+
+skaitlis = randint(1, 10)
+
+while skaitlis != 10 :
+    print('Tests')
+
+print('Programma darbu beidz.')
